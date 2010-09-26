119669482bed7b08e486e834839779cce55180c4
Christopher Nelson <nadiasvertex@gmail.com>
2010-09-26 14:40:15 -0400

    Fill/Spill logic now compiles - but doesn't run.

--- src/python/ssa.go
+++ src/python/ssa.go
@@ -27,7 +27,10 @@ import (
 )
 
 const (
-    SSA_LOAD = iota
+    SSA_CALL = iota
+    SSA_SPILL
+    SSA_FILL
+    SSA_LOAD 
     SSA_STORE
     SSA_ADD
     SSA_SUB
@@ -42,7 +45,6 @@ const (
     SSA_GET
     SSA_SET
     SSA_IDX
-    SSA_CALL   
 )
 
 const (
@@ -93,6 +95,18 @@ type SsaElement struct {
     Register    int
 }
 
+// Helps to track items which had to be spilled
+// from the register bank during register allocation.
+type SsaSpill struct {
+    Free    *vector.IntVector
+    Map     map[*SsaElement]int
+}
+
+func (s *SsaSpill) Init() {
+    s.Free = new (vector.IntVector)
+    s.Map = make(map[*SsaElement]int, 8)
+}
+
 type SsaContext struct {
     LastElementId   int
     Elements        []*SsaElement
@@ -110,6 +124,10 @@ type SsaContext struct {
     FloatIdx        map[float64]int
     StringIdx       map[string]int        
     NameIdx         map[string]int
+    
+    // How many slots are needed for some
+    // code object in order to spill
+    SpillRoomNeeded int
 }
 
 func (ctx *SsaContext) Init() {
@@ -178,6 +196,28 @@ func (ctx *SsaContext) Eval(op uint, src1, src2 int) int {
     return ctx.Write(el)
 }
 
+func (ctx *SsaContext) Spill(to_slot, from_register int) int {
+
+    el := new (SsaElement)
+    
+    el.Op = SSA_SPILL
+    el.Src1 = to_slot
+    el.Register = from_register
+    
+    return ctx.Write(el)
+}
+
+func (ctx *SsaContext) Fill(from_slot, to_register int) int {
+
+    el := new (SsaElement)
+    
+    el.Op = SSA_FILL
+    el.Src1 = from_slot
+    el.Register = to_register
+    
+    return ctx.Write(el)
+}
+
 func (ctx *SsaContext) LoadInt(v *big.Int) int {
     idx, present := ctx.IntIdx[v]
     
@@ -203,12 +243,109 @@ func (ctx *SsaContext) LoadInt(v *big.Int) int {
     return idx
 }
 
+// Generates a spill instruction.  Decides what to spill, and generates an instruction to save
+// the spilled value.  The return value is the newly freed register.  
+func (ctx *SsaContext) generateSpill(active_elements *vector.Vector,  spill_mag *SsaSpill) int {
+
+    // Find a register to spill.  Our heuristic is to
+    // choose the register with the longest lifetime. That
+    // seems counter-intuitive, but http://www.cs.ucla.edu/~palsberg/course/cs132/linearscan.pdf
+    // indicates that it performs best.  Assuming I understood the
+    // paper, of course.
+    var spill_el *SsaElement = nil
+    spilled_el_index := 0
+    
+    for i:=0; i<active_elements.Len(); i++ {
+       can_el :=  active_elements.At(i).(*SsaElement)
+       
+       // If we don't have an element to spill yet, or if the current
+       // element is a better candidate, choose it.
+       if spill_el == nil || spill_el.LiveEnd < can_el.LiveEnd {
+        spill_el = can_el
+        spilled_el_index = i       
+       }
+    }    
+    
+    free_slot := 0
+    
+    // Once we've chose a register, we need to figure out where to spill the
+    // data to.  We try to make this reasonably optimal, but we can grow the
+    // spill area as needed.  (Something not true about our register set. :-D)
+    if spill_mag.Free.Len() == 0 {
+        // No free spill slots, grow it.
+        free_slot = len(spill_mag.Map)
+    } else {
+        free_slot = spill_mag.Free.Pop()
+    }    
+    
+    spill_mag.Map[spill_el] = free_slot
+    
+    // Now emit a spill instruction
+    // so that we don't lose the work done.            
+    ctx.Spill(free_slot, spill_el.Register)
+        
+    // Make sure to track how much spill room is needed
+    if ctx.SpillRoomNeeded < len(spill_mag.Map) {
+        ctx.SpillRoomNeeded = len(spill_mag.Map)
+    }
+                
+    // Remove it from the active list
+    active_elements.Delete(spilled_el_index)
+    
+    // Return the newly freed register number
+    return spill_el.Register
+}
+
+// Generates a fill instruction.  Previously the value must have been spilled out to the save area.  An
+// instruction is emitted to load it back into the register set.  Other registers may be spilled in order
+// to bring the spilled value back in.  Returns the id of the element that generated the fill.  This id
+// should be used as the new source value of an SsaElement that depends on the spilled value.
+func (ctx *SsaContext) generateFill(el *SsaElement, active_elements *vector.Vector, spill_mag *SsaSpill, free_regs *vector.IntVector) int {
+    // Figure out where the element was 
+    // spilled to.
+    free_slot := spill_mag.Map[el]
+    spill_mag.Free.Push(free_slot)
+    
+    // Activate the element.
+    active_elements.Push(el)
+    
+    target_reg := 0
+    
+    // Find a free register (possibly by spilling another register.)
+    if free_regs.Len() == 0 {
+        target_reg = ctx.generateSpill(active_elements, spill_mag)            
+    } else {
+        target_reg = free_regs.Pop()
+    }    
+    
+    // Write the fill instruction
+    return ctx.Fill(free_slot, target_reg)    
+}
+
+
 // Performs a linear-scan allocation of registers.  Only one pass is used to allocate registers to all
 // SSA instructions.
 func (ctx *SsaContext) AllocateRegisters(num_regs int) {
 
+    // We create a new context so that we can rewrite the SSA stream into it.  This is because
+    // we expect that we will need to spill at least one SSA into a temporary space.  A possible
+    // future optimization of this code would be to hve the Strahler number calculated by the
+    // AST traversal phase so we know if we will need to spill or not.  Of course, we also take
+    // this opportunity to do some optimizations that require rewriting the stream anyway (like 
+    // dead code elimination.)
+    
+    new_ctx := new(SsaContext)
+    new_ctx.Init()
+    
+    // Tracks old_ssa_id -> new_ssa_id values so
+    // we can rename the parameters correctly during rewrite.
+    rename_map := make(map[int]int, 16)
+
     // The list of free regs is kept here
     free_regs := new(vector.IntVector)
+        
+    // The list of spilled elements is kept here
+    spill_mag := new(SsaSpill)
     
     // Push all the registers except 0 onto the free list. We assume the 0 register
     // is reserved for the 0 value, thus it is never available.
@@ -256,16 +393,38 @@ func (ctx *SsaContext) AllocateRegisters(num_regs int) {
      
         // Figure out what register it should go into
         if free_regs.Len() == 0 {
-            // Oh noes!! we are out of registers! we need to find a new free one.
-            panic("Ran out of registers!!")
+            el.Register = new_ctx.generateSpill(active_elements, spill_mag)            
         } else {
             el.Register = free_regs.Pop()
-        }
+        }        
         
-                    
+        // Process any renames and fills
+        if el.Op > SSA_STORE {	        
+	        // Check for (and perfoem) any needed renames.
+	        if new_src1_name, present := rename_map[el.Src1]; present {
+	            el.Src1 = new_src1_name
+	        }
+	        
+	        if new_src2_name, present := rename_map[el.Src2]; present {
+	            el.Src2 = new_src2_name
+	        }
+	        
+	        // Check to see if we need to fill some registers from the
+	        // spill area in order to process this instruction.  If so, 
+	        // we _may_ need to spill one or two registers in order to
+	        // have the space we need to fill for this instruction.
+	        left_el := new_ctx.Elements[el.Src1]
+	        if _, spilled := spill_mag.Map[left_el]; spilled {
+	           el.Src1 = new_ctx.generateFill(left_el, active_elements, spill_mag, free_regs)
+	        }
+	        
+	        right_el := new_ctx.Elements[el.Src2]
+            if _, spilled := spill_mag.Map[right_el]; spilled {
+               el.Src2 = new_ctx.generateFill(right_el, active_elements, spill_mag, free_regs)
+            }
+        }
+                
+        // Write the possibly renamed element into the new context
+        rename_map[ssa_id] = new_ctx.Write(el)                    
     }    
-}
-
-func (ctx *SsaContext) Dump() {
-    
 }
